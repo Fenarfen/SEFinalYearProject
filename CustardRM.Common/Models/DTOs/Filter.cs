bb60508bf0d81cffa0bd99d559e3017bee67df21
Lewis Taylor <lewis.graham.taylor@gmail.com>
2025-04-04 00:48:12 +0100

    Implemented basic /inventory

---
+++ CustardRM.Common/Models/DTOs/Filter.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustardRM.Common.Models.DTOs;
+
+public class Filter
+{
+	public class Field
+	{
+		public bool Enabled { get; set; } = false;
+	}
+	public class FilterField<T> : Field
+	{
+		public T Value { get; set; }
+	}
+
+	public class RangeFilterField<T> : Field
+	{
+		public T LowerValue { get; set; }
+		public T UpperValue { get; set; }
+	}
+	public class StockItemFilter
+	{
+		public FilterField<string> ItemCodeFilter { get; set; } = new();
+		public FilterField<string> ItemNameFilter { get; set; } = new();
+		public FilterField<string> DescriptionFilter { get; set; } = new();
+		public RangeFilterField<decimal?> UnitPriceFilter { get; set; } = new();
+		public RangeFilterField<decimal?> CostPriceFilter { get; set; } = new();
+		public FilterField<bool> IsActiveFilter { get; set; } = new();
+		public RangeFilterField<DateTime?> CreatedAtFilter { get; set; } = new();
+		public RangeFilterField<DateTime?> UpdatedAtFilter { get; set; } = new();
+
+		public int GetEnabledCount()
+		{
+			int count = 0;
+			foreach (var prop in this.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+			{
+				var propValue = prop.GetValue(this);
+				if (propValue == null)
+					continue;
+
+				var enabledProp = prop.PropertyType.GetProperty("Enabled");
+				if (enabledProp != null)
+				{
+					var enabledValue = enabledProp.GetValue(propValue);
+					if (enabledValue is bool isEnabled && isEnabled)
+					{
+						count++;
+					}
+				}
+			}
+			return count;
+		}
+	}
+}
---
+++ CustardRM.Common/Models/Entities/Inventory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustardRM.Common.Models.Entities;
+
+public class Inventory
+{
+    public class StockItem
+    {
+        public int ID { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal CostPrice { get; set; }
+        public int StockLevel { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class Review
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public int StockItemID { get; set; }
+        public string Body { get; set; }
+        public float Rating { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class Subcategory
+    {
+        public int ID { get; set; }
+        public int CategoryID { get; set; }
+        public string SubcategoryName { get; set; }
+        public string SubcategoryDescription { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
