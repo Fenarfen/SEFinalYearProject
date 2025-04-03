5fac65ee2a49b5274c696d95888c55d54e78a7fb
Lewis Taylor <lewis.graham.taylor@gmail.com>
2025-04-04 00:47:15 +0100

    Refactored http client

---
+++ CustardRM.Common/Interfaces/IHttpClientService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustardRM.Common.Interfaces
+{
+    public interface IHttpClientService
+    {
+        Task<HttpResponseMessage> SendGet(string url);
+        Task<HttpResponseMessage> SendPost(string url, object data);
+    }
+}
--- CustardRM.Frontend/Program.cs
+++ CustardRM.Frontend/Program.cs
@@ -1,13 +1,20 @@
 using CustardRM.Frontend.Components;
 using CustardRM.Common.Interfaces;
 using CustardRM.Common.Services;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<HttpClientService>((sp, client) =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+    var baseUri = config.GetValue<string>("BaseURI")
+        ?? throw new Exception("Cannot find BaseURI in appsettings.json");
+    client.BaseAddress = new Uri(baseUri);
+});
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ICookieService, CookieService>();
